fcc1145f726f375586bc4bb9ba19a908f78a6301
Sruthika890 <siripurapusruthika.24.cse@anits.edu.in>
2026-07-17 21:59:25 +0530

    Add program to calculate string length

---
+++ Calculate the length of the string.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include<string>
+using namespace std;
+int main() {
+    string s;
+    cin>>s;
+    cout<<s.length();
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
